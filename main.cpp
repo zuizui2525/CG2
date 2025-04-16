696ac8a29da1e42c873bdfea05a6e84bc4cb2576
zuizui2525 <k024g0006@g.neec.ac.jp>
2025-04-16 17:19:38 +0900

    00_06完了

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
 #include <cassert>
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
+#include <dbghelp.h>
+#pragma comment(lib, "Dbghelp.lib")
+#include <strsafe.h>
 #pragma warning(pop)
 
 // string->wstring変換
@@ -71,8 +74,47 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
+	// 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	wchar_t filePath[MAX_PATH] = { 0 };
+	CreateDirectory(L"./Dumps",nullptr);
+	StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d%02d-%02d%02d.dmp", time.wYear, time.wYear, time.wDay, time.wHour, time.wMinute);
+	HANDLE dumpFileHandle = CreateFile(
+		filePath, // ファイル名
+		GENERIC_READ | GENERIC_WRITE, // 読み書き
+		FILE_SHARE_WRITE | FILE_SHARE_READ, // 書き込みと読み込みの共有
+		0, // セキュリティ属性
+		CREATE_ALWAYS, // 常に新規作成
+		0, // 属性
+		0); // テンプレートファイル
+	// processId(このexeのId)とクラッシュ(例外)の発生したthreadIdを取得
+	DWORD processId = GetCurrentProcessId();
+	DWORD threadId = GetCurrentThreadId();
+	// 設定情報を入力
+	MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+	minidumpInformation.ThreadId = threadId; // スレッドID
+	minidumpInformation.ExceptionPointers = exception; // 例外情報
+	minidumpInformation.ClientPointers = TRUE; // クライアントポインタを使う
+	// Dumpを出力。MiniDumpNormalは最小限の情報を出力するフラグ
+	MiniDumpWriteDump(
+		GetCurrentProcess(), // プロセスハンドル
+		processId, // プロセスID
+		dumpFileHandle, // ダンプファイルハンドル
+		MiniDumpNormal, // ダンプの種類
+		&minidumpInformation, // 例外情報
+		nullptr, // スレッド情報
+		nullptr); // ユーザーデータ
+	// 他に関連図けられているSEH例外ハンドラがあれば実行。通常はプロセスを終了する
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// 誰も補足しなかった場合に(Unhandled)、補足する関数を登録
+	SetUnhandledExceptionFilter(ExportDump);
+	
 	// ログのディレクトリ
 	std::filesystem::create_directory("logs");
 	// 現在時刻を取得
@@ -177,6 +219,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// デバイスが生成できなかったので起動できない
 	assert(device != nullptr);
 	Log(logStream, "Complete create D3D12Device!!!\n"); // 初期化完了のログを出す
+
+	//uint32_t* p = nullptr;
+	//*p = 100;
 
 	MSG msg{};
 	// ウィンドウの×ボタンが押されるまでループ
