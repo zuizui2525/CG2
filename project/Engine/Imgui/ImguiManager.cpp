a172640f619a7040a666bf7b0b9160cace3a33c4
zuizui2525 <k024g0006@g.neec.ac.jp>
2025-11-10 16:45:29 +0900

    Imguiのクラス化完了

---
+++ project/Engine/Imgui/ImguiManager.cpp
@@ -0,0 +1,40 @@
+#ifdef _DEBUG
+#include "ImguiManager.h"
+
+void ImguiManager::Initialize(HWND hwnd, ID3D12Device* device, int backBufferCount,
+    DXGI_FORMAT rtvFormat, ID3D12DescriptorHeap* rtvHeap, ID3D12DescriptorHeap* srvHeap) {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX12_Init(device,
+        backBufferCount,
+        rtvFormat,
+        rtvHeap,
+        srvHeap->GetCPUDescriptorHandleForHeapStart(),
+        srvHeap->GetGPUDescriptorHandleForHeapStart());
+
+    initialized_ = true;
+}
+
+void ImguiManager::Begin() {
+    if (!initialized_) return;
+    ImGui_ImplDX12_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+}
+
+void ImguiManager::End() {
+    if (!initialized_) return;
+    ImGui::Render();
+}
+
+void ImguiManager::Shutdown() {
+    if (!initialized_) return;
+    ImGui_ImplDX12_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+    initialized_ = false;
+}
+#endif
