d4fc40d5ee2018ce7cf506bffe678747b60dda60
zuizui2525 <casa01732925@gmail.com>
2026-01-13 12:25:20 +0900

    リークエラー

--- project/ZuizuiEngine/Zuizui.cpp
+++ project/ZuizuiEngine/Zuizui.cpp
@@ -1,1 +1,89 @@
 #include "Zuizui.h"
+#include "ModelObject.h"
+#include "SphereObject.h"
+#include "SpriteObject.h"
+#include "TriangleObject.h"
+#include "ParticleManager.h"
+#include "ImguiManager.h"
+
+Zuizui* Zuizui::instance = nullptr;
+
+Zuizui* Zuizui::GetInstance() {
+    if (!instance) instance = new Zuizui();
+    return instance;
+}
+
+void Zuizui::Initialize() {
+    window.Initialize(L"LE2B_02_イトウカズイ");
+    window.Show();
+    dxCommon.Initialize(window.GetHWND(), WindowApp::kClientWidth, WindowApp::kClientHeight);
+
+    psoManager = std::make_unique<PSOManager>(dxCommon.GetDevice());
+    psoManager->Initialize(dxCommon.GetDxcUtils(), dxCommon.GetDxcCompiler(), dxCommon.GetIncludeHandler());
+
+    textureManager = std::make_unique<TextureManager>();
+    textureManager->Initialize(dxCommon.GetDevice(), dxCommon.GetCommandList(), dxCommon.GetSrvHeap());
+
+    input = std::make_unique<Input>();
+    input->Initialize(window.GetInstance(), window.GetHWND());
+
+    camera = std::make_unique<Camera>();
+    camera->Initialize(dxCommon.GetDevice(), input.get());
+
+    dirLight = std::make_unique<DirectionalLightObject>();
+    dirLight->Initialize(dxCommon.GetDevice());
+
+    audio.Initialize();
+
+#ifdef _DEBUG
+    imgui = std::make_unique<ImguiManager>();
+    imgui->Initialize(window.GetHWND(), dxCommon.GetDevice(), dxCommon.GetBackBufferCount(), dxCommon.GetRtvFormat(), dxCommon.GetRtvHeap(), dxCommon.GetSrvHeap());
+#endif
+}
+
+void Zuizui::BeginFrame() {
+    dxCommon.FrameStart();
+#ifdef _DEBUG
+    imgui->Begin();
+#endif
+    dxCommon.BeginFrame();
+    dxCommon.PreDraw();
+}
+
+void Zuizui::EndFrame() {
+#ifdef _DEBUG
+    imgui->End();
+    dxCommon.DrawImGui();
+#endif
+    dxCommon.EndFrame();
+    dxCommon.FrameEnd(60);
+}
+
+// 描画ラップ関数群
+void Zuizui::DrawModel(ModelObject* model, const std::string& textureKey, bool drawFlag) {
+    if (!drawFlag || !model) return;
+    model->Draw(dxCommon.GetCommandList(), textureManager->GetGpuHandle(textureKey), dirLight->GetGPUVirtualAddress(), camera->GetGPUVirtualAddress(), psoManager->GetPSO("Object3D"), psoManager->GetRootSignature("Object3D"), true);
+}
+
+void Zuizui::DrawSphere(SphereObject* sphere, const std::string& textureKey, bool drawFlag) {
+    if (!drawFlag || !sphere) return;
+    sphere->Draw(dxCommon.GetCommandList(), textureManager->GetGpuHandle(textureKey), dirLight->GetGPUVirtualAddress(), camera->GetGPUVirtualAddress(), psoManager->GetPSO("Object3D"), psoManager->GetRootSignature("Object3D"), true);
+}
+
+void Zuizui::DrawParticle(ParticleManager* particle, const std::string& textureKey, bool drawFlag) {
+    if (!drawFlag || !particle) return;
+    particle->Draw(dxCommon.GetCommandList(), textureManager->GetGpuHandle(textureKey), dirLight->GetGPUVirtualAddress(), psoManager->GetPSO("Particle"), psoManager->GetRootSignature("Particle"), true);
+}
+
+void Zuizui::Finalize() {
+    psoManager.reset();
+    textureManager.reset();
+    camera.reset();
+    dirLight.reset();
+    input.reset();
+#ifdef _DEBUG
+    imgui->Shutdown();
+#endif
+    // COMの終了処理
+    CoUninitialize();
+}
--- project/ZuizuiEngine/Zuizui.h
+++ project/ZuizuiEngine/Zuizui.h
@@ -1,11 +1,66 @@
 #pragma once
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include "DxCommon.h"
+#include "WindowApp.h"
+#include "PSOManager.h"
+#include "TextureManager.h"
+#include "Camera.h"
+#include "DirectionalLight.h"
+#include "Log.h"
+#include "Input.h"
+#include "Audio.h"
+
+// 前方宣言
+class ModelObject;
+class SpriteObject;
+class SphereObject;
+class TriangleObject;
+class ParticleManager;
+
 class Zuizui {
 public:
-	void Initialize();
-	void Begin();
-	void BeginFrame();
-	void EndFrame();
-	void Finalize();
-private:
+    static Zuizui* GetInstance();
 
+    // 基盤の初期化と終了
+    void Initialize();
+    void Finalize();
+
+    // フレーム制御
+    bool ProcessMessage() { return window.ProcessMessage(); }
+    void BeginFrame();
+    void EndFrame();
+
+    // --- 簡略化された描画関数 ---
+    void DrawModel(ModelObject* model, const std::string& textureKey, bool drawFlag = true);
+    void DrawSphere(SphereObject* sphere, const std::string& textureKey, bool drawFlag = true);
+    void DrawSprite(SpriteObject* sprite, const std::string& textureKey, bool drawFlag = true);
+    void DrawTriangle(TriangleObject* triangle, const std::string& textureKey, bool drawFlag = true);
+    void DrawParticle(ParticleManager* particle, const std::string& textureKey, bool drawFlag = true);
+
+    // ゲッター
+    ID3D12Device* GetDevice() { return dxCommon.GetDevice(); }
+    DxCommon* GetDxCommon() { return &dxCommon; }
+    Camera* GetCamera() { return camera.get(); }
+    PSOManager* GetPSOManager() { return psoManager.get(); }
+    TextureManager* GetTextureManager() { return textureManager.get(); }
+
+private:
+    Zuizui() = default;
+    static Zuizui* instance;
+
+    WindowApp window;
+    Log logger;
+    DxCommon dxCommon;
+    Audio audio;
+    std::unique_ptr<Input> input;
+    std::unique_ptr<PSOManager> psoManager;
+    std::unique_ptr<TextureManager> textureManager;
+    std::unique_ptr<Camera> camera;
+    std::unique_ptr<DirectionalLightObject> dirLight;
+
+#ifdef _DEBUG
+    std::unique_ptr<class ImguiManager> imgui;
+#endif
 };
